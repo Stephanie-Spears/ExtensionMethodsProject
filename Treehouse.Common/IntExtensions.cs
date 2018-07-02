feac1ede1622fe8ed28f57d2a486161f7ed53bab
Stephanie Spears <stephanie.spears1@gmail.com>
2018-07-01 20:42:31 -0700

    add bitwise operation for boolean IsEven extension method

--- Treehouse.Common/IntExtensions.cs
+++ Treehouse.Common/IntExtensions.cs
@@ -8,7 +8,7 @@ namespace Treehouse.Common
     {
         public static bool IsEven(this int value)
         {
-            return (value % 2) == 0;
+            return (value & 1) == 0;
         }
     }
 }
