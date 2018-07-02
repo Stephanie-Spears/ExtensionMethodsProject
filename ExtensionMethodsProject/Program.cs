7d583facd90677c11864f6e0250547ed30dbef83
Stephanie Spears <stephanie.spears1@gmail.com>
2018-07-01 18:37:12 -0700

    add common library and reference

--- ExtensionMethodsProject/Program.cs
+++ ExtensionMethodsProject/Program.cs
@@ -1,17 +1,35 @@
 using System;
-using System.Collections.Generic;
+using Treehouse.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExtensionMethodsProject
+namespace Treehouse.ExtensionMethodsProject
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            var synonymnsForBest = new List<string>
+            {
+                "best",
+                "finest",
+                "greatest",
+                "top",
+                "foremost",
+                "leading",
+                "most excellent",
+                "preeminent",
+                "premier",
+                "chief",
+                "supreme",
+                "unrivaled",
+                "perfect",
+                "incomparable",
+                "ideal"
+            };
+            Console.WriteLine($"My kitty Little Kitty was the {synonymnsForBest.RandomItem()} cat.");
+            Console.ReadLine();
         }
     }
 }
